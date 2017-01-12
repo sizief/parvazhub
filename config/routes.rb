dd8e843312ca0691751fb62487e6d2bbeb956b8a
sizief <sizief@gmail.com>
2017-01-12 19:03:14 +0330

    add abouts us and static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/about-us', to:'static_pages#about_us'
+
   post '/search', to:'search#search_proccess'
   get '/test', to:'search#test'
   root 'search#flight'
