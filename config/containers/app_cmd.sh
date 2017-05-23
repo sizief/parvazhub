c36a8be8301493ae9db34343878e20c0e90c22d8
Ali Deishidi <sizief@gmail.com>
2017-05-23 14:29:21 +0430

    add password to redis

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -3,7 +3,7 @@
 # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
 #exec bundle exec unicorn -c config/containers/unicorn.rb -E $RAILS_ENV;
 #exec bundle exec puma -C config/containers/puma.rb; 
-bundle exec sidekiq #-r app/workers/search_worker.rb #-L log/sidekiq.log
+#bundle exec sidekiq #-r app/workers/search_worker.rb #-L log/sidekiq.log
 
 if [ "$ENTRYPOINT" = "app" ]
 then
@@ -11,7 +11,7 @@ then
 
 elif [ "$ENTRYPOINT" = "background_job" ]
 then
-  exec bundle exec puma -C config/containers/puma.rb; 
+  #exec bundle exec puma -C config/containers/puma.rb; 
   exec bundle exec sidekiq #-r app/workers/search_worker.rb #-L log/sidekiq.log
 
 fi
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS']}
+  config.redis = { url: ENV['REDIS'], password: ENV["REDIS_PASSWORD"]}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS']}
+  config.redis = { url: ENV['REDIS'], password: ENV["REDIS_PASSWORD"]}
 end
 
 #SearchWorker.perform_in(5.seconds,'thr','mhd',"2017-05-29")
