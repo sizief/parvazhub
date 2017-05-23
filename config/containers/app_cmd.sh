eb5c6027fa690b1667f305848dcbbf510164c296
Ali Deishidi <sizief@gmail.com>
2017-05-23 11:50:04 +0430

    change CMD docker for non docker compose mode

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -2,4 +2,16 @@
 
 # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
 #exec bundle exec unicorn -c config/containers/unicorn.rb -E $RAILS_ENV;
-exec bundle exec puma -C config/containers/puma.rb; 
+#exec bundle exec puma -C config/containers/puma.rb; 
+bundle exec sidekiq #-r app/workers/search_worker.rb #-L log/sidekiq.log
+
+if [ "$ENTRYPOINT" = "app" ]
+then
+  exec bundle exec puma -C config/containers/puma.rb; 
+
+elif [ "$ENTRYPOINT" = "background_job" ]
+then
+  exec bundle exec puma -C config/containers/puma.rb; 
+  exec bundle exec sidekiq #-r app/workers/search_worker.rb #-L log/sidekiq.log
+
+fi
