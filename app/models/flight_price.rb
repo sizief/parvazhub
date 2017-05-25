85d03095c5fdc64cefe1ff1cb8a695373b2838f5
Ali Deishidi <sizief@gmail.com>
2017-05-25 20:42:33 +0430

    add price view table

--- app/models/flight_price.rb
+++ app/models/flight_price.rb
@@ -22,6 +22,17 @@ class FlightPrice < ApplicationRecord
     	prices
     end
 
+    def get_lowest_price_time_table(origin,destination,date)
+        route = Route.find_by(origin:origin, destination:destination)
+        dates = {twodaybefore: (date.to_date-2).to_s, onedaybefore: (date.to_date-1).to_s, theday: (date.to_date).to_s, dayafter: (date.to_date+1).to_s, twodayafter: (date.to_date+2).to_s}
+        prices = {twodaybefore:'', onedaybefore:'',theday:'', dayafter: '', twodayafter: ''}
+
+        dates.each do |title,date|
+            prices[title.to_sym] = get_lowest_price(route,date)
+        end
+        prices
+    end
+
     def get_lowest_price(route,date)
         flight = route.flights.where(departure_time: date.to_datetime.beginning_of_day.to_s..date.to_datetime.end_of_day.to_s).where.not(best_price:0).sort_by(&:best_price).first
         flight
