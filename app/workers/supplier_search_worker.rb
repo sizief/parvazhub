002c8ed173fa32c1d3565e273f8066eb0c75db00
Ali Deishidi <sizief@gmail.com>
2017-07-11 18:48:10 +0430

    test to fix restclient thread safe bug - add timeout to searchworker

--- app/workers/supplier_search_worker.rb
+++ app/workers/supplier_search_worker.rb
@@ -5,10 +5,10 @@ class SupplierSearchWorker
   sidekiq_options :retry => 2, :backtrace => true, :queue => 'critical'
  
   def perform(supplier_name, supplier_class,origin,destination,route_id,date)
-    #Timeout.timeout((ENV["SUPPLIER_TIMEOUT"].to_f)*2) do
+    Timeout.timeout((ENV["SUPPLIER_TIMEOUT"].to_f)*2) do
       x = SupplierSearch.new
       x.search_supplier(supplier_name,supplier_class,origin,destination,route_id,date)
-    #end
+    end
   end
 
 end
