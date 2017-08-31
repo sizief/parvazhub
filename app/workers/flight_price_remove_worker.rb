a68ebcd8232a5870e18b1c020df4c85420032d16
Ali Deishidi <sizief@gmail.com>
2017-08-31 11:57:07 +0430

    update flight price remover

--- app/workers/flight_price_remove_worker.rb
+++ app/workers/flight_price_remove_worker.rb
@@ -10,8 +10,7 @@ class FlightPriceRemoveWorker
     {date:(Date.today+3).to_s,outdate_minutes:370},
     {date:(Date.today+4).to_s,outdate_minutes:370},
     {date:(Date.today+5).to_s,outdate_minutes:370},
-    {date:(Date.today+6).to_s,outdate_minutes:370},
-    {date:(Date.today+7).to_s,outdate_minutes:370}
+    {date:(Date.today+6).to_s,outdate_minutes:370}
  ]
 
   def perform
