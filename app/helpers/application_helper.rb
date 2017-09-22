e96428f5daca56d0199ffd04620ad65cbdebb6f5
Ali Deishidi <sizief@gmail.com>
2017-09-22 10:09:41 +0330

    fix indent on app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
  
-    def week_day_to_human_persian index
+  def week_day_to_human_persian index
    days = ["","دوشنبه","سه‌شنبه","چهارشنبه","پنج‌شنبه","جمعه","شنبه","یک‌شنبه"]
    return days[index]
- end
+  end
 
 end
