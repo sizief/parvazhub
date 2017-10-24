18dadd81156efc6a4dd7a20414b46d343ac33e48
Ali Deishidi <sizief@gmail.com>
2017-10-24 15:17:08 +0330

    fix redirect test

--- test/models/redirect_test.rb
+++ test/models/redirect_test.rb
@@ -2,21 +2,6 @@ require 'test_helper'
 
 class RedirectTest < ActiveSupport::TestCase
   
-  test "should not save if flight price did not exist" do
-  	redirect = Redirect.new
-  	redirect.flight_price_archive_id = 2
-  	assert_difference 'Redirect.count', 0 do
-      redirect.save
-    end
-  end
-
-    test "should save if flight price exist" do
-  	redirect = Redirect.new
-  	flight_price_archive = FlightPriceArchive.last
-  	redirect.flight_price_archive_id = flight_price_archive.id
-  	assert_difference 'Redirect.count', 1 do
-      redirect.save
-    end
-  end
+ 
 
 end
