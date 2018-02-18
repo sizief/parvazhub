4548aca50c1372b66c5adaeb1a11c569905e2d01
Ali Deishidi <sizief@gmail.com>
2018-02-19 01:28:39 +0330

    add access origin for API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,15 @@
 class ApiController < ApplicationController
+  after_filter :cors_set_access_control_headers
+
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+  
     
     def city_prefetch_suggestion
       city_list = Array.new
