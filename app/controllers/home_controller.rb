542339a9e990f3de806753bbc9ff25fc77d4a0f7
Ali Deishidi <sizief@gmail.com>
2018-04-05 20:08:12 +0430

    add last search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,44 @@ class HomeController < ApplicationController
       end
     end
     @is_mobile = browser.device.mobile? 
+    @search_parameter = user_last_search current_user
+  end
+
+  private
+  def user_last_search user
+    if user.nil?
+      params = default_search_params
+    else 
+      params = user_search_params user
+    end
+  end
+
+  def user_search_params user
+    origin = City.find_by(city_code: user.user_search_histories.last.route.origin)
+    destination = City.find_by(city_code: user.user_search_histories.last.route.destination)
+    date = user.user_search_histories.last.departure_time
+    fill_params origin, destination, date
+  end
+
+  def default_search_params
+    origin = City.find_by(city_code: "thr")
+    destination = City.find_by(city_code: "mhd")
+    date = (Date.today+1).to_s
+    {origin_english_name: origin.english_name, 
+    origin_code: origin.city_code,
+    destination_english_name: destination.english_name, 
+    destination_code: destination.city_code,
+    date: date}
+  end
+
+  def fill_params origin, destination, date
+    {origin_english_name: origin.english_name, 
+    origin_persian_name: origin.persian_name, 
+    origin_code: origin.city_code,
+    destination_english_name: destination.english_name, 
+    destination_persian_name: destination.persian_name, 
+    destination_code: destination.city_code,
+    date: date}
   end
 
 end
