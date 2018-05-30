6d367eccf834b3967f2288c68586305399245556
Ali Deishidi <sizief@gmail.com>
2018-05-30 21:44:10 +0430

    fix numbers on chart

--- app/helpers/en/route_helper.rb
+++ app/helpers/en/route_helper.rb
@@ -4,4 +4,8 @@ module En::RouteHelper
     raw price_dates.map{|item| ((item[:date].to_date == Date.today)? "Today" : item[:date].to_date.strftime("%A %-d %B")) }
   end
 
+  def en_prices_for_chart price_dates
+    price_dates.map{|item| (item[:price_dollar].nil? ? 0 : item[:price_dollar])}
+  end
+
 end
