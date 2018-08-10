1e07413c55ef41bba599f72f47505ca453b76afa
Ali Deishidi <sizief@gmail.com>
2018-08-10 16:31:19 +0430

    add foreman to capistarno

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,23 +4,23 @@ server ENV["SSH_IP"], port: ENV["SSH_PORT"], roles: [:web, :app, :db], primary: 
 set :repo_url,        'git@github.com:sizief/parvazhub.git'
 set :application,     'parvazhub'
 set :user,            ENV["SSH_USER"]
-#set :puma_threads,    [4, 16]
-#set :puma_workers,    0
-
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/#{fetch(:application)}"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+
 #set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 #set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 #set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 #set :puma_access_log, "#{release_path}/log/puma.error.log"
 #set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 #set :puma_preload_app, true
 #set :puma_worker_timeout, nil
 #set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+#set :puma_threads,    [4, 16]
+#set :puma_workers,    0
 
 ## Defaults:
 # set :scm,           :git
@@ -46,14 +46,6 @@ namespace :deploy do
     end
   end
 
-#   desc 'Initial Deploy'
-#   task :initial do
-#     on roles(:app) do
-#       before 'deploy:restart', 'puma:start'
-#       invoke 'deploy'
-#     end
-#   end
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -67,70 +59,39 @@ namespace :deploy do
   #after  :finishing,    :restart
 end
 
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's systemd scripts"
+  task :export, :roles => :app do
+    run "cd /var/www/parvazhub/current && sudo foreman export --app parvazhub --user sizief systemd /etc/systemd/system/"
+  end
+  
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "sudo systemctl start parvazhub.target"
+  end
+
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "sudo systemctl stop parvazhub.target"
+  end
+
+  desc "Restart the server services"
+  task :systemd_restart, :roles => :app do
+    run "systemctl daemon-reload"
+  end
+
+  desc "Restart the server nginx"
+  task :nginx_restart, :roles => :app do
+    run "sudo systemctl restart nginx.service"
+  end
+end
+
+after "deploy:update", "foreman:export"
+after "deploy:update", "foreman:systemd_restart"
+after "deploy:update", "foreman:stop"
+after "deploy:update", "foreman:start"
+after "deploy:update", "foreman:nginx_restart"
+
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
-
-
-
-
-
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
-
-
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
