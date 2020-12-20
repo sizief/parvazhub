e3c44f118552ba6a865575da3b4f3a92b9f8665e
Ali Deishidi <sizief@gmail.com>
2020-12-21 02:44:05 +0330

    fix migrations

--- db/migrate/20201220192735_add_default_to_proxies.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultToProxies < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :proxies, :enable
-    add_column :proxies, :enable, :boolean, default: true
-  end
-end
--- db/migrate/20201220222108_add_default_for_enable_proxies.rb
+++ db/migrate/20201220222108_add_default_for_enable_proxies.rb
@@ -1,6 +1,6 @@
 class AddDefaultForEnableProxies < ActiveRecord::Migration[5.2]
   def change
-    remove_column :proxies, :enable
+    remove_column :proxies, :status
     add_column :proxies, :enable, :boolean, default: true
   end
 end
