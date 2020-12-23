eb57d427732f4cf83cf8285ebecf783bcf40cfa3
Ali Deishidi <sizief@gmail.com>
2020-12-24 02:08:20 +0330

    fix hipo test

--- test/models/suppliers/hipotrip_test.rb
+++ test/models/suppliers/hipotrip_test.rb
@@ -6,36 +6,38 @@ class SuppliersHipotripTest < ActiveSupport::TestCase
   def setup
     @origin = 'thr'
     @destination = 'mhd'
-    @date = '2017-12-20'
-    @search_history_id = 1
-    @route = Route.find_by(origin: @origin, destination: @destination)
-    @search_flight_token = 1
-    @supplier_name = 'Hipotrip'
-    @hipotrip_obj = Suppliers::Hipotrip.new(origin: @origin,
-                                            destination: @destination,
-                                            route: @route,
-                                            date: @date,
-                                            search_history_id: @search_history_id,
-                                            search_flight_token: @search_flight_token,
-                                            supplier_name: @supplier_name)
+    date = '2017-12-20'
+    supplier_name = 'Hipotrip'
+    route = Route.find_or_create_by(origin: @origin, destination: @destination)
+    search_history = SearchHistory.create(supplier_name: supplier_name, route: route)
+    search_flight_token = 1
+    @hipotrip = Suppliers::Hipotrip.new(
+      origin: @origin,
+      destination: @destination,
+      route: route,
+      date: date,
+      search_history_id: search_history.id,
+      search_flight_token: search_flight_token,
+      supplier_name: supplier_name
+    )
   end
 
   test 'search supplier' do
-    response = @hipotrip_obj.search_supplier
+    response = @hipotrip.search_supplier
     assert response.is_a? Hash
     assert_not response[:response].empty?
   end
 
   test 'get params' do
-    response = @hipotrip_obj.get_params
+    response = @hipotrip.get_params
     assert response.is_a? Hash
   end
 
   test 'to minutes' do
-    time1 = @hipotrip_obj.to_minutes '03:30'
-    time2 = @hipotrip_obj.to_minutes '3:30'
-    time3 = @hipotrip_obj.to_minutes '3:00'
-    time4 = @hipotrip_obj.to_minutes '0:30'
+    time1 = @hipotrip.to_minutes '03:30'
+    time2 = @hipotrip.to_minutes '3:30'
+    time3 = @hipotrip.to_minutes '3:00'
+    time4 = @hipotrip.to_minutes '0:30'
 
     assert_equal time1, 210
     assert_equal time2, 210
@@ -44,16 +46,16 @@ class SuppliersHipotripTest < ActiveSupport::TestCase
   end
 
   test 'import flights' do
-    response = @hipotrip_obj.search_supplier
+    response = @hipotrip.search_supplier
     assert_difference 'Flight.count', 27 do
-      @hipotrip_obj.import_flights(response)
+      @hipotrip.import_flights(response)
     end
   end
 
   test 'save flight prices' do
-    response = @hipotrip_obj.search_supplier
+    response = @hipotrip.search_supplier
     assert_difference 'FlightPrice.count', 27 do
-      @hipotrip_obj.import_flights(response)
+      @hipotrip.import_flights(response)
     end
   end
 end
--- test/models/suppliers/trip_test.rb
+++ test/models/suppliers/trip_test.rb
@@ -11,7 +11,7 @@ class SuppliersTripTest < ActiveSupport::TestCase
     @supplier_name = 'Trip'
     @search_history = SearchHistory.create(supplier_name: @supplier_name, route: @route)
     @search_flight_token = 1
-    @trip_obj = Suppliers::Trip.new(
+    @trip = Suppliers::Trip.new(
       origin: @origin,
       destination: @destination,
       route: @route,
@@ -38,33 +38,33 @@ class SuppliersTripTest < ActiveSupport::TestCase
 
   test 'send request for register' do
     url = 'https://www.trip.ir/flex/search'
-    response = @trip_obj.send_request(@request_type, url, @params)
+    response = @trip.send_request(@request_type, url, @params)
     assert response.is_a? String
     assert_equal JSON.parse(response)['success'], true
   end
 
   test 'register request' do
-    response = @trip_obj.register_request(@origin, @destination, @date)
+    response = @trip.register_request(@origin, @destination, @date)
     assert response.is_a? Hash
     assert response['sid'].is_a? String
   end
 
   test 'is search complete' do
     id = 1
-    response = @trip_obj.is_search_complete(id)
+    response = @trip.is_search_complete(id)
     assert_equal response, true
   end
 
   test 'trip search should return Hash' do
-    response = @trip_obj.search_supplier
+    response = @trip.search_supplier
     assert response.is_a? Hash
     assert_not response[:response].empty?
   end
 
   test 'Save flights to database' do
-    response = @trip_obj.search_supplier
+    response = @trip.search_supplier
     assert_difference 'Flight.count', 47 do
-      @trip_obj.import_flights(response)
+      @trip.import_flights(response)
     end
   end
 
@@ -80,14 +80,14 @@ class SuppliersTripTest < ActiveSupport::TestCase
       supplier_name: @supplier_name
     ).search_supplier
     assert_difference 'Flight.count', 127 do
-      @trip_obj.import_flights(response)
+      @trip.import_flights(response)
     end
   end
 
   test 'Save flight prices to database' do
-    response = @trip_obj.search_supplier
+    response = @trip.search_supplier
     assert_difference 'FlightPrice.count', 47 do
-      @trip_obj.import_flights(response)
+      @trip.import_flights(response)
     end
   end
 
@@ -95,7 +95,7 @@ class SuppliersTripTest < ActiveSupport::TestCase
     arrivals = ['2017-11-19 18:00:00'.to_datetime, '2017-11-19 20:00:00'.to_datetime, '2017-11-19 23:00:00'.to_datetime]
     departures = ['2017-11-19 14:00:00'.to_datetime, '2017-11-19 19:00:00'.to_datetime, '2017-11-19 22:00:00'.to_datetime]
 
-    assert_equal  @trip_obj.calculate_stopover_duration(['2017-11-19 18:00:00'.to_datetime], ['2017-11-19 18:00:00'.to_datetime]), 0
-    assert_equal  @trip_obj.calculate_stopover_duration(departures, arrivals), 180
+    assert_equal  @trip.calculate_stopover_duration(['2017-11-19 18:00:00'.to_datetime], ['2017-11-19 18:00:00'.to_datetime]), 0
+    assert_equal  @trip.calculate_stopover_duration(departures, arrivals), 180
   end
 end
