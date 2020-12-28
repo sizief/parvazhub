31766074a1d26d1521eaf9360b1abfb6aca7882b
Ali Deishidi <sizief@gmail.com>
2020-12-28 11:58:13 +0330

    increase thread max

--- config/puma.rb
+++ config/puma.rb
@@ -18,7 +18,7 @@ pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 8 }.to_i
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
