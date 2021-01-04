eedbae47d3e09c9f1c041b81469c9283f822f38f
Ali Deishidi <sizief@gmail.com>
2021-01-04 15:22:35 +0330

    Revert "remove Flytoday"
    This reverts commit 1cde6823d3969f94867978517007e53f2ccd433d.

---
+++ app/services/suppliers/flightio.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+class Suppliers::Flightio < Suppliers::Base
+  DEEPLINK = ENV['URL_FLIGHTIO_DEEPLINK']
+  URL = ENV['URL_FLIGHTIO_GET']
+  USERNAME = ENV['FLIGHTIO_USERNAME']
+  PASSWORD = ENV['FLIGHTIO_PASSWORD']
+
+  def register_request
+    update_status("using proxy: #{proxy}") unless proxy.nil?
+    JSON.parse(
+      RestClient::Request.execute(
+        method: :post,
+        url: URL,
+        proxy: nil,
+        headers: headers,
+        payload: { "ValueObject": params.to_json.to_s }.to_json
+      ).body
+    )
+  rescue *HTTP_ERRORS => e
+    update_status(e)
+    nil
+  end
+
+  def search_supplier
+    registered_request = register_request
+    return { status: false } if registered_request.nil? || !registered_request['IsSuccessful']
+
+    update_status("using proxy: #{proxy}") unless proxy.nil?
+    request_id = registered_request['Data']
+    deep_link = "#{DEEPLINK + request_id}&CombinationID="
+    value = "/?value={%22FSL_Id%22:%22#{request_id}%22,%22PagingModel%22:{%22Page%22:1,%22Size%22:30,%22SortColumn%22:%22TotalChargeable%22,%22SortDirection%22:%220%22}}"
+    response = RestClient::Request.execute(
+      method: :get,
+      url: URL + value,
+      proxy: nil,
+      headers: headers
+    )
+    { status: true, response: response.body, deeplink: deep_link }
+  rescue *HTTP_ERRORS => e
+    update_status(e)
+    { status: false }
+  end
+
+  def import_flights(response)
+    JSON.parse(response[:response])['ResultModel']['ItemList'].each do |flight_item|
+      flight = flight_item['Items'].first['Segments'].first
+      airline_code = get_airline_code(flight['OperatingAirlineCode'])
+      flight_number = airline_code + flight['FlightNumber']
+      departure_time = "#{flight['DepartTime'][0..9]} #{flight['DepartTime'][11..]}"
+      airplane_type = flight['AircraftName']
+      price = flight_item['TotalChargeable'].to_f / 10
+      deeplink_url = response[:deeplink] + flight_item['CombinationID']
+
+      saved_flight = Flight.upsert(route.id, flight_number, departure_time, airline_code, airplane_type)
+      add_to_flight_prices(
+        FlightPrice.new(flight_id: saved_flight.id, price: price.to_s, supplier: supplier_name.downcase, flight_date: date.to_s, deep_link: deeplink_url)
+      )
+    end
+  end
+
+  def get_airline_code(airline_code)
+    airlines = {
+      'RV' => 'IV',
+      'IS' => 'SR'
+    }
+    airlines[airline_code].nil? ? airline_code : airlines[airline_code]
+  end
+
+  def params
+    date_time_string = "#{date}T00:00:00"
+    {
+      "ADT": 1,
+      "CHD": 0,
+      "INF": 0,
+      "CabinType": '1',
+      "flightType": '2',
+      "tripMode": '1',
+      "DestinationInformationList": [
+        {
+          "DepartureDate": date_time_string.to_s,
+          "DestinationLocationAirPortCode": destination.upcase.to_s,
+          "DestinationLocationAllAirport": true,
+          "DestinationLocationCityCode": destination.upcase.to_s,
+          "Index": 1,
+          "OriginLocationAirPortCode": origin.upcase.to_s,
+          "OriginLocationAllAirport": true,
+          "OriginLocationCityCode": origin.upcase.to_s
+        }
+      ]
+    }
+  end
+
+  def headers
+    {
+      "FUser": USERNAME,
+      "FPass": PASSWORD,
+      "FApiVersion": '1.1',
+      "Content-Type": 'application/json'
+      # "FSession": '078ee89c-0cdd-43c8-b414-a4fe1113079a',
+      # "Host": 'api.flightio.com',
+      # "User-Agent": 'Dalvik/2.1.0 (Linux; U; Android 10; E6883 Build/QQ3A.200805.001)',
+      # "Content-Type": 'application/json; charset=UTF-8',
+      # "connection": 'close',
+      # "Accept-Encoding": 'gzip'
+    }
+  end
+
+  def proxy
+    @proxy ||= Proxy.fetch_path(supplier_name)
+  end
+end
---
+++ app/services/suppliers/flytoday.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+class Suppliers::Flytoday < Suppliers::Base
+  def get_params
+    shamsi_date = JalaliDate.new(date.to_date).strftime '%Y-%m-%d'
+    params = { 'OriginLocationCodes[0]' => origin.upcase.to_s,
+               'DestinationLocationCodes[0]' => destination.upcase.to_s,
+               'DepartureDateTimes[0]' => shamsi_date.to_s,
+               'DepartureDateTimes_lang[0]' => '',
+               'DepartureDateTimeR' => '',
+               'AdultCount' => 1,
+               'ChildCount' => 0,
+               'InfantCount' => 0,
+               'CabinType' => 100,
+               'Foreign_FlightType' => 'OneWay' }
+  end
+
+  def search_supplier
+    url = ENV['URL_FLYTODAY_SEARCH']
+    response = RestClient::Request.execute(method: :post,
+                                               url: URI.parse(url).to_s,
+                                               payload: get_params,
+                                               proxy: nil)
+    response = response.body
+    { status: true, response: response }
+  rescue StandardError => e
+    update_status(e.message)
+    { status: false }
+  end
+
+  def import_flights(response)
+    flight_id = nil
+    flight_prices = []
+    flight_ids = []
+    json_response = JSON.parse(response[:response])
+
+    json_response['PricedItineraries'][0..ENV['MAX_NUMBER_FLIGHT'].to_i].each do |flight|
+      leg_data = flight_id = nil
+      leg_data = prepare flight['FlightSegments']
+
+      next if leg_data.nil?
+
+      stops = leg_data[:stop].empty? ? nil : leg_data[:stop].join(',')
+      ActiveRecord::Base.connection_pool.with_connection do
+        flight_id = Flight.create_or_find_flight(route.id,
+                                                 leg_data[:flight_number].join(','),
+                                                 leg_data[:departure_date_time].first,
+                                                 leg_data[:airline_code].join(','),
+                                                 leg_data[:airplane_type].join(','),
+                                                 leg_data[:arrival_date_time].last,
+                                                 stops,
+                                                 leg_data[:trip_duration])
+      end
+      flight_ids << flight_id
+
+      price = flight['PtcFareBreakdowns'][0]['TotalFareWithMarkupAndVat']
+      price = (price / 10).to_i
+      deeplink_url = get_deeplink(flight['FareSourceCode'])
+
+      # to prevent duplicate flight prices we compare flight prices before insert into database
+      flight_price_so_far = flight_prices.select { |flight_price| flight_price.flight_id == flight_id }
+      unless flight_price_so_far.empty? # check to see a flight price for given flight is exists
+        if flight_price_so_far.first.price.to_i <= price.to_i # saved price is cheaper or equal to new price so we dont touch it
+          next
+        else
+          flight_price_so_far.first.price = price # new price is cheaper, so update the old price and go to next price
+          flight_price_so_far.first.deep_link = deeplink_url
+          next
+        end
+      end
+
+      flight_prices << FlightPrice.new(flight_id: flight_id.to_s, price: price.to_s, supplier: supplier_name.downcase, flight_date: date.to_s, deep_link: deeplink_url.to_s)
+    end # end of each loop
+
+    complete_import(flight_prices)
+    flight_ids
+  end
+
+  def prepare(flight_legs)
+    flight_numbers = []
+    airline_codes = []
+    airplane_types = []
+    departure_date_times = []
+    arrival_date_times = []
+    stops = []
+    trip_duration = 0
+    flight_legs.each_with_index do |leg, index|
+      airline_code = leg['OperatingAirline']
+      flight_number = (leg['FlightNumber'].include? airline_code) ? leg['FlightNumber'] : airline_code + leg['FlightNumber']
+
+      flight_numbers << flight_number
+      airline_codes << airline_code
+      airplane_types << leg['OperatingEquipment']
+      departure_date_times << parse_date(leg['DepartureDateTime']).utc.to_datetime + ENV['IRAN_ADDITIONAL_TIMEZONE'].to_f.minutes # add 4:30 hours because flytoday date time is in iran time zone #.strftime("%H:%M")
+      arrival_date_times << parse_date(leg['ArrivalDateTime']).utc.to_datetime + ENV['IRAN_ADDITIONAL_TIMEZONE'].to_f.minutes
+      unless flight_legs.count == index + 1
+        stops << leg['ArrivalAirport']
+      end # we dont need last stops
+      trip_duration += to_minutes leg['JourneyDuration']
+    end
+
+    trip_duration += calculate_stopover_duration(departure_date_times, arrival_date_times)
+    { flight_number: flight_numbers,
+      airline_code: airline_codes,
+      airplane_type: airplane_types,
+      departure_date_time: departure_date_times,
+      arrival_date_time: arrival_date_times,
+      stop: stops,
+      trip_duration: trip_duration }
+  end
+
+  def to_minutes(time_string)
+    total_time = 0
+    time_string = time_string.split(':')
+    total_time = time_string.first.to_i * 60
+    total_time += time_string.second.to_i
+  end
+
+  def parse_date(datestring)
+    seconds_since_epoch = datestring.scan(/[0-9]+/)[0].to_i / 1000.0
+    Time.at(seconds_since_epoch)
+  end
+
+  def get_deeplink(fare_source_code)
+    ENV['URL_FLYTODAY_DEEPLINK'] + fare_source_code
+  end
+end
