bc4d3375857b8f570486dbd9e8ed5f002ad8e7fc
Ali Deishidi <sizief@gmail.com>
2022-05-22 21:49:40 +0200

    remove sentry and add comment on reviews

--- app/services/handle_error.rb
+++ app/services/handle_error.rb
@@ -3,7 +3,5 @@
 class HandleError
   def self.call(error)
     raise error if Rails.env.development?
-
-    Sentry.capture_message(error) if Rails.env.production?
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,13 +12,3 @@ module FlightSearch
     config.assets.enabled = true
   end
 end
-
-Sentry.init do |config|
-  config.dsn = 'https://3223b53cbeba430faf119a09dba0f99d@o495243.ingest.sentry.io/5567670'
-  config.breadcrumbs_logger = [:active_support_logger]
-
-  # To activate performance monitoring, set one of these options.
-  # We recommend adjusting the value in production:
-  config.traces_sample_rate = 1
-  config.enabled_environments = %w[production]
-end
